0dc7dea595d93be97527d65ef3b6a1fe413929d6
Adam Kendis <atkendis@gmail.com>
2019-09-15 12:01:09 -0700

    Moved getCoords to App component. Removed geolocation module file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { getGps } from '../modules/geolocation.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.getCoords.bind(this);
   };
 
+  getCoords() {
+    if (navigator.geolocation) {
+      const success = (pos) => {
+        const {latitude, longitude, altitude, accuracy, altitudeAccuracy, heading} = pos.coords;
+        const timestamp = new Date(pos.timestamp).toLocaleString();
+        const locInfo = {latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, timestamp};
+        this.setState(locInfo);
+      }
+      const error = (err) => {
+        console.warn(err.code, ': ', err.message);
+      }
+      const options = {
+        maximumAge: 15000,
+        enableHighAccuracy: true,
+        timeout: 5000,
+      }
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+      alert('Geolocation is not supported on this device.')
+    }
+  }
+
   componentDidMount() {
-    let coords = getGps();
-    // console.log(coords)
-    this.setState(coords);
+    this.getCoords();
   }
   
   render() {
